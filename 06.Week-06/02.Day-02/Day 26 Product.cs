6ef1ad279b372b6e47a390dbedfedd5fdf0b6beb
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-25 15:29:00 +0530

    Add files via upload

---
+++ 06.Week-06/02.Day-02/Day 26 Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
