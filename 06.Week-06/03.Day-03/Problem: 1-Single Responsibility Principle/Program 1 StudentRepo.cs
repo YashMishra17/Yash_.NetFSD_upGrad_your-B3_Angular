5c528102735429003008576d9f4b444a4ca90171
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-27 15:56:54 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 1-Single Responsibility Principle/Program 1 StudentRepo.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class StudentRepository
+    {
+        private List<Student> students = new List<Student>();
+
+        // Add student
+        public void AddStudent(Student s)
+        {
+            students.Add(s);
+        }
+
+        // Get all students
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+    }
+}
