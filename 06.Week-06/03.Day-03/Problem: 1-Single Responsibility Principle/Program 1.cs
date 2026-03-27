8063b6129f90ac18e6ef9b84abaaed201bb90cc4
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-27 15:57:40 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 1-Single Responsibility Principle/Program 1.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleApp1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            StudentRepository repo = new StudentRepository();
+            ReportGenerator report = new ReportGenerator();
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add Student");
+                Console.WriteLine("2. View Report");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Student s = new Student();
+
+                    Console.Write("Enter ID: ");
+                    s.StudentId = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Name: ");
+                    s.StudentName = Console.ReadLine();
+
+                    Console.Write("Enter Marks: ");
+                    s.Marks = Convert.ToInt32(Console.ReadLine());
+
+                    repo.AddStudent(s);
+                    Console.WriteLine("Student added successfully!");
+                }
+                else if (choice == 2)
+                {
+                    var students = repo.GetStudents();
+
+                    if (students.Count == 0)
+                    {
+                        Console.WriteLine("No data available.");
+                    }
+                    else
+                    {
+                        report.GenerateReport(students);
+                    }
+                }
+                else if (choice == 3)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice!");
+                }
+            }
+        }
+    }
+}
