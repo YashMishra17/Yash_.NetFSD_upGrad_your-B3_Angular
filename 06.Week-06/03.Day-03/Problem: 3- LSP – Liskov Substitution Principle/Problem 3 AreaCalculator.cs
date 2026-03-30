8e95f8e26c4b14285831e002ac8530a2a6acdefd
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 16:10:24 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 3- LSP – Liskov Substitution Principle/Problem 3 AreaCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class AreaCalculator
+    {
+        public void PrintArea(Shape shape)
+        {
+            Console.WriteLine("Area: " + shape.CalculateArea());
+        }
+    }
+}
