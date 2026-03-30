201caea8667b8f1a880f2decf86f183767bf7ec4
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 16:11:53 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 3- LSP – Liskov Substitution Principle/Problem 3 Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            AreaCalculator calculator = new AreaCalculator();
+
+            // Rectangle
+            Shape rect = new Rectangle
+            {
+                Width = 5,
+                Height = 4
+            };
+
+            calculator.PrintArea(rect);
+
+            // Circle
+            Shape circle = new Circle
+            {
+                Radius = 3
+            };
+
+            calculator.PrintArea(circle);
+
+            Console.ReadLine();
+        }
+    }
+}
