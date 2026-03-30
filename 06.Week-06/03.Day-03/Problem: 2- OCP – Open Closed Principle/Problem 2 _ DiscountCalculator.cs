2f57af0d1a4e3ab52b8427a1ef1cf409e633a311
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 15:31:17 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 2- OCP – Open Closed Principle/Problem 2 _ DiscountCalculator.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class DiscountCalculator
+    {
+        public double GetFinalPrice(double amount, IDiscountStrategy strategy)
+        {
+            double discount = strategy.CalculateDiscount(amount);
+            return amount - discount;
+        }
+    }
+}
