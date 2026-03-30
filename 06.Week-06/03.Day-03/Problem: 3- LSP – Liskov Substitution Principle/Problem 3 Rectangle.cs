42719b58d462ceaf858f21d00dd6acdef12b5660
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 16:12:15 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 3- LSP – Liskov Substitution Principle/Problem 3 Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+}
