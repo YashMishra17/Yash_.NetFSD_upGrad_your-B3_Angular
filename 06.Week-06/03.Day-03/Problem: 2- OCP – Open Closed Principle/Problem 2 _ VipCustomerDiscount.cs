04e549252ef5883a6282170faa3abd6af260ff6e
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 15:30:37 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 2- OCP – Open Closed Principle/Problem 2 _ VipCustomerDiscount.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class VipCustomerDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double amount)
+        {
+            return amount * 0.20; // 20%
+        }
+    }
+}
