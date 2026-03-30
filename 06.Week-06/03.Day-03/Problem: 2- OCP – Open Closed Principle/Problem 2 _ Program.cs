a26c9222a4f7e2d32a6318c1ae72c1fd113df5f7
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 15:31:43 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 2- OCP – Open Closed Principle/Problem 2 _ Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            DiscountCalculator calculator = new DiscountCalculator();
+
+            Console.Write("Enter Amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose Customer Type:");
+            Console.WriteLine("1. Regular");
+            Console.WriteLine("2. Premium");
+            Console.WriteLine("3. VIP");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            IDiscountStrategy strategy = choice switch
+            {
+                1 => new RegularCustomerDiscount(),
+                2 => new PremiumCustomerDiscount(),
+                3 => new VipCustomerDiscount(),
+                _ => null
+            };
+
+            if (strategy == null)
+            {
+                Console.WriteLine("Invalid choice");
+                return;
+            }
+
+            double finalPrice = calculator.GetFinalPrice(amount, strategy);
+
+            Console.WriteLine($"Final Price: {finalPrice}");
+            Console.ReadLine();
+        }
+    }
+}
