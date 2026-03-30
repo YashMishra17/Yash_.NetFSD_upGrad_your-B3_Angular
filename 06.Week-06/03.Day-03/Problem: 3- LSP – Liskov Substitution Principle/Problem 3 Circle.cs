468eebe2fa7dffa4439c63d4910e6f6004a8439c
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-30 16:11:33 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem: 3- LSP – Liskov Substitution Principle/Problem 3 Circle.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
