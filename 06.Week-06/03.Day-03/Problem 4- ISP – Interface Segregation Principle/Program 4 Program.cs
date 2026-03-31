2d7e96347485d382b68f54a9e9045550d4f33810
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-31 14:42:54 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem 4- ISP – Interface Segregation Principle/Program 4 Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Basic Printer
+            IPrinter basic = new BasicPrinter();
+            basic.Print();
+
+            Console.WriteLine();
+
+            // Advanced Printer
+            AdvancedPrinter advanced = new AdvancedPrinter();
+            advanced.Print();
+            advanced.Scan();
+            advanced.Fax();
+
+            Console.ReadLine();
+        }
+    }
+}
