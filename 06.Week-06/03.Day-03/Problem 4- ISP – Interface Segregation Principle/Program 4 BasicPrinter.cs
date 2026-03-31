abd74787a5f28fcc541580ecc486e936eed61bee
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-31 14:40:23 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem 4- ISP – Interface Segregation Principle/Program 4 BasicPrinter.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class BasicPrinter : IPrinter
+    {
+        public void Print()
+        {
+            Console.WriteLine("Basic Printer: Printing document...");
+        }
+    }
+}
