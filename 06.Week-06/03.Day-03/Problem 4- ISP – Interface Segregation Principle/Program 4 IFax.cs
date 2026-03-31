208c0d51f736a81d8e0efa72e7c6e36f08b1d536
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-31 14:40:45 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem 4- ISP – Interface Segregation Principle/Program 4 IFax.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public interface IFax
+    {
+        void Fax();
+    }
+}
