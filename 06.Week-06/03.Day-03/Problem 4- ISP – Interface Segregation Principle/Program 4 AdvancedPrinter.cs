3b263497e61b207adf76428a4d86d380edc5c850
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-03-31 14:39:11 +0530

    Add files via upload

---
+++ 06.Week-06/03.Day-03/Problem 4- ISP – Interface Segregation Principle/Program 4 AdvancedPrinter.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class AdvancedPrinter : IPrinter, IScanner, IFax
+    {
+        public void Print()
+        {
+            Console.WriteLine("Advanced Printer: Printing document...");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Advanced Printer: Scanning document...");
+        }
+
+        public void Fax()
+        {
+            Console.WriteLine("Advanced Printer: Sending fax...");
+        }
+    }
+}
