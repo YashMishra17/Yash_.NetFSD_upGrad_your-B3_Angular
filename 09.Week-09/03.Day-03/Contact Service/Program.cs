d0d421da2784462b10ca2f80ef80f4d1d9a61055
Yash Mishra <110155847+YashMishra17@users.noreply.github.com>
2026-04-16 21:59:56 +0530

    Add files via upload

---
+++ 09.Week-09/03.Day-03/Contact Service/Program.cs
@@ -0,0 +1,39 @@
+using ContactService.Data;
+using ContactService.Repositories;
+using ContactService.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactService, ContactService.Services.ContactService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 09.Week-09/03.Day-03/Contact Service/Services/IContactService.cs
@@ -0,0 +1,12 @@
+﻿using ContactService.Models;
+
+namespace ContactService.Services;
+
+public interface IContactService
+{
+    Task<IEnumerable<Contact>> GetAll();
+    Task<Contact> GetById(int id);
+    Task<Contact> Add(Contact contact);
+    Task<Contact> Update(int id, Contact contact);
+    Task<bool> Delete(int id);
+}
